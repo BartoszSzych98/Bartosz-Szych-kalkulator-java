f9023098a1b57ae0d868c4169e301b8291423951
Bartosz Szych <bartoszych17@gmail.com>
2022-11-19 23:20:24 +0100

    6.2 Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
+import com.kodilla.testing.Calculatorr.Calculatorr;
 
 public class TestingMain {
-        public static void main(String[] args) {
-            System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+    public static void main(String[] args)  {
+
+        Calculatorr calculator = new Calculatorr();
+        int a=10;
+        int b=17;
+
+        int result = calculator.add(10, 17);
+        if (result == 27) {
+            System.out.println("Add test is OK :)");
+        } else {
+            System.out.println("Error!");
+        }
+
+        result = calculator.subtract(10, 17);
+        if (result == -7) {
+            System.out.println("Subtract test is OK :)");
+        } else {
+            System.out.println("Error!");
         }
     }
+}
+
