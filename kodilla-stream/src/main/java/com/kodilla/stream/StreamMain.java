8a0303d6c365c472c5b30059c7a1b829144b688c
Bartosz Szych <bartoszych17@gmail.com>
2022-12-02 13:57:59 +0100

    Zadanie 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,12 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -13,6 +19,17 @@ public class StreamMain {
 
         System.out.println("\nUsing Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(10);
+
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> theResultMapOfUsers = forum.getList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> forumUser.getDateOfBirth().isBefore(LocalDate.of(1998, 03, 18)))
+                .filter(forumUser -> forumUser.getNumberOfposts() > 0)
+                .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
+
+        for (Map.Entry entry : theResultMapOfUsers.entrySet()) {
+            System.out.println( entry);
+        }
 
     }
 }
